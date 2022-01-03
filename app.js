8540a0a0140122f382659a17603c04f55f5656c3
prasanna <prasannaramesh98@gmail.com>
2022-01-03 15:26:30 +0530

    update a function

---
+++ app.js
@@ -0,0 +1,16 @@
+function computerPlay(){
+    let result = " ";
+    let computerPick = Math.floor(Math.random()*3) +1;
+
+    if(computerPick === 1){
+        result ="rock";
+    }
+    else if(computerPick === 2){
+        result ="paper";
+    }
+    else if(computerPick === 3){
+        result ="scissors";
+    }else{
+        result ="error";
+    }
+}
