8eb832177307541d717992f43a2fe154c59b8c2e
prasanna <prasannaramesh98@gmail.com>
2022-02-23 10:44:07 +0530

    half done with HTML and CSS part

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-function computerPlay(){
-    let result = " ";
-    let computerPick = Math.floor(Math.random()*3) +1;
+// function computerPlay(){
+//     let result = " ";
+//     let computerPick = Math.floor(Math.random()*3) +1;
 
-    if(computerPick === 1){
-        result ="rock";
-    }
-    else if(computerPick === 2){
-        result ="paper";
-    }
-    else if(computerPick === 3){
-        result ="scissors";
-    }else{
-        console.log("error");
-    }
-    return result;
-}
+//     if(computerPick === 1){
+//         result ="rock";
+//     }
+//     else if(computerPick === 2){
+//         result ="paper";
+//     }
+//     else if(computerPick === 3){
+//         result ="scissors";
+//     }else{
+//         console.log("error");
+//     }
+//     return result;
+// }
