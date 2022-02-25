8ecaa3d388d07183375a76e61d86f33c87191fca
prasanna <prasannaramesh98@gmail.com>
2022-02-25 19:05:38 +0530

    done with basic r-p-s

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const btnSelection = document.querySelectorAll('[data-selection]');
-const 
-
+const finalColoumn = document.querySelector('[data-final-column]');
+const yourScoreSpan = document.querySelector('[data-your-score]');
+const computerScoreSpan = document.querySelector('[data-computer-score]');
 const gamePlay = [
     {name:'rock', emoji:'🗿', beats: 'scissors'},
     {name:'paper', emoji:'📄', beats: 'rock'},
     {name:'scissors', emoji:'✂', beats: 'paper'}
 ]
+
+
 
 // playerPlay
 btnSelection.forEach(buttonSelection=>{
@@ -17,20 +20,34 @@ btnSelection.forEach(buttonSelection=>{
 });
 
 function playerSelection(selection){
-    const computerSelection = randomSelection
+    const computerSelection = randomSelection()
     const yourWinner = isWinner(selection, computerSelection)
     const computerWinner = isWinner(computerSelection, selection)
     console.log(selection);
     addSelectionResult(computerSelection, computerWinner);
     addSelectionResult(selection, yourWinner);
 
+    if(yourWinner) scoreIncrement(yourScoreSpan)
+    if(computerWinner) scoreIncrement(computerScoreSpan)
+
+}
+// scores
+
+function scoreIncrement(scoreSpan){
+    scoreSpan.innerText = parseInt(scoreSpan.innerText) + 1
+
 }
 
-addSelectionResult(){
-
+function addSelectionResult(selection, winner){
+    const div = document.createElement('div')
+    div.innerText = selection.emoji
+    div.classList.add('result-selection')
+    if(winner) div.classList.add('winner')
+    
+    finalColoumn.after(div)
 }
 
-function isWinner(selection, opponetSelection){
+function isWinner(selection, opponentSelection){
     return selection.beats === opponentSelection.name
 
 }
